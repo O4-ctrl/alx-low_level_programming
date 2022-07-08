94a70929e463dca640fa1fd9eea1c3fdb12c8ab9
Ibrahim Abdulrazaq <ozoyaibrahim@gmail.com>
2022-07-07 19:49:57 -0700

    My commit message

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - printz "Fizz" for factor of 3,
+ * main - prints "Fizz" for factor of 3,
  * prints "Buzz" for factor of 5,
  * prints "FizzBuzz" for factor of both 3 and 5
  * for numbers 1-100
@@ -9,20 +9,20 @@
  */
 int main(void)
 {
-int x;
+	int x;
 
-for (x = 1; x <= 99; x++)
-{
-if (x % 15 == 0)
-printf("FizzBuzz ");
-else if (x % 3 == 0)
-printf("Fizz");
-else if (x % 5 == 0)
-printf("Buzz");
-else
-printf("%i ", x);
-}
+	for (x = 1; x <= 99; x++)
+	{
+		if (x % 15 == 0)
+			printf("FizzBuzz ");
+		else if (x % 3 == 0)
+			printf("Fizz");
+		else if (x % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%i ", x);
+	}
 
-printf("Buzz\n");
-return (0);
+	printf("Buzz\n");
+	return (0);
 }
