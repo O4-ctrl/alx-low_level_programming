3d53afab608514e45e72d1805349f5f0ad63c756
Ibrahim Abdulrazaq <ozoyaibrahim@gmail.com>
2022-07-07 19:20:47 -0700

    My commit message

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,4 @@
-#include  "main.h"
+#include "main.h"
 
 /**
  * print_most_numbers - print 0 to 9 without 2 and 4.
@@ -7,18 +7,18 @@
  */
 void print_most_numbers(void)
 {
-int a;
+	int a;
 
-for (a = 0; a <= 9; a++)
-{
-if (a ==âœ 2 || a == 4)
-{
-continue;
-}
-else
-{
-_putchar('0' + a);
-}
-}
-_putchar('\n');
+	for (a = 0; a <= 9; a++)
+	{
+		if (a == 2 || a == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar('0' + a);
+		}
+	}
+	_putchar('\n');
 }
