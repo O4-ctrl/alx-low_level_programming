04451257a16d637492d5f6af3f6e095be9535e93
Ibrahim Abdulrazaq <ozoyaibrahim@gmail.com>
2022-07-14 10:42:47 -0700

    My commit message

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * string_toupper - changes all lowercase to capital letters
- * @st: string to modify
+ * string_toupper - changes all lowercase letter of a string to uppercase
+ * @str: string to modify
  * Return: char
  */
-char *string_toupper(char *st)
+char *string_toupper(char *str)
 {
 int i;
 
-for (i = 0; st[i] != '\0'; i++)
+for (i = 0; str[i] != '\0'; i++)
 {
-if (st[i] >= 'a' && st[i] <= 'z')
-st[i] -Â= 32;
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] -= 32;
 }
 
-return (st);
+return (str);
 }
